b8f265280820eb064f09a2bf4c3f72e32f9b9eb5
mohanthilak <mohan.thilak410@gmail.com>
2021-09-08 11:59:53 +0530

    fadsadfs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./app.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
@@ -10,6 +10,7 @@ import SignUp from "./pages/signUp/SignUp";
 import Questions from "./pages/questions/Questions";
 import Chat from "./pages/Chat/Chat";
 import Profile from "./pages/Profile/Profile";
+import axios from "axios";
 import io from "socket.io-client";
 import useIsLoggedIn from "./useIsLoggedIn";
 const socket = io.connect("https://quizzooo.herokuapp.com/");
@@ -18,6 +19,20 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useIsLoggedIn("isLoggedIn", false);
   //   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:4000/getuser",
+    }).then((res) => {
+      if (res.data.isLoggedIn) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
   return (
     <Router>
       <LoginContext.Provider
